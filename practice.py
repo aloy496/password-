51d3233bd7248ad6dd9a94a263150b41b346a947
aloy496 <aloysiouskamya99@gmail.com>
2026-04-17 13:14:40 +0300

    Actually adding line8 after saving

--- practice.py
+++ practice.py
@@ -4,4 +4,5 @@ if len(sys.argv) < 2:
 for arg in sys.argv[1:]:
     print("Hello my name is", arg) 
     print("Hello my name is", arg) 
+    print("Hello my name is", arg)
     print("Hello my name is", arg) 
